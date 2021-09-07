fb50025a26029b7053388efa69d563645438cdfd
Tigris <tabula.tenebris@gmail.com>
2021-09-08 03:52:23 +0600

    Add project files.

---
+++ FunWithArrays/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunWithArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fun with arrays");
+            SimpleArrays();
+            ArrayInit();
+            DeclareImplicitArrays();
+            ArrayOfObjects();
+            RectMultidimensionalArray();
+            JaggedMultidimensionalArray();
+            Console.ReadLine();
+        }
+
+        static void SimpleArrays()
+        {
+            Console.WriteLine("=> Simple Array Creation.");
+
+            int[] myInts = new int[3];
+            myInts[0] = 100;
+            myInts[1] = 200;
+            myInts[2] = 300;
+
+            foreach (int i in myInts)
+            {
+                Console.WriteLine(i);
+            }
+
+            string[] booksOnDotNet = new string[100];
+            Console.WriteLine();
+        }
+
+        static void ArrayInit()
+        {
+            Console.WriteLine("=>Array initialization.");
+
+            string[] stringArray = new string[] { "one", "two", "three" };
+            Console.WriteLine("stringArray has {0} elements", stringArray.Length);
+
+            bool[] boolArray = { false, true, false };
+            Console.WriteLine("boolArray has {0} elements", boolArray.Length);
+
+            int[] intArray = new int[4] { 20, 22, 23, 0 };
+            Console.WriteLine("intArray has {0} elements", intArray.Length);
+            Console.WriteLine();
+        }
+
+        static void DeclareImplicitArrays()
+        {
+            Console.WriteLine("=>Implicit Array Initialization.");
+
+            var a = new[] { 1, 10, 100, 1000 };
+            Console.WriteLine("a is a: {0}", a.ToString());
+
+            var b = new[] { 1, 1.5, 2, 2.5 };
+            Console.WriteLine("b is a: {0}", b.ToString());
+
+            var c = new[] { "hello", null, "world" };
+            Console.WriteLine("c is a: {0}", c.ToString());
+            Console.WriteLine();
+        }
+
+        static void ArrayOfObjects()
+        {
+            object[] myObjects = new object[4];
+            myObjects[0] = 10;
+            myObjects[1] = false;
+            myObjects[2] = new DateTime(1969, 3, 24);
+            myObjects[3] = "Form & Void";
+            foreach (object obj in myObjects)
+            {
+                Console.WriteLine("Type: {0}, Value: {1}", obj.GetType(), obj);
+            }
+            Console.WriteLine();
+        }
+
+        static void RectMultidimensionalArray()
+        {
+            Console.WriteLine("=>Rectangular multidimensional array.");
+
+            int[,] myMatrix = new int[3, 4];
+
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 4; j++)
+                    myMatrix[i, j] = i * j;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                    Console.Write(myMatrix[i, j] + "\t");
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+
+        static void JaggedMultidimensionalArray()
+        {
+            Console.WriteLine("=>Jagged multidimensional array.");
+
+            int[][] myJagArray = new int[5][];
+
+            for (int i = 0; i < myJagArray.Length; i++)
+            {
+                myJagArray[i] = new int[i + 7];
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < myJagArray[i].Length; j++)
+                {
+                    Console.Write(myJagArray[i][j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
